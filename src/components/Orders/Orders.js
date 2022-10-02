1925d3ef363a9a4b5f7e6b8b74550fbccdf609a9
EmranSWE <mdemran.swe@gmail.com>
2022-10-02 13:49:01 +0600

    backend server is connected with client side

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -9,17 +9,17 @@ import './Orders.css'
 const Orders = () => {
     const [products,setProducts]=useProducts();
     const navigate=useNavigate();
-    const [cart,setCart]=useCart(products);
+    const [cart,setCart]=useCart();
     const handleRemoveProduct=product =>{
-        const rest=cart.filter(pd =>pd.id !==product.id);
+        const rest=cart.filter(pd =>pd._id !==product._id);
         setCart(rest);
-        removeFromDb(product.id)
+        removeFromDb(product._id)
     }
     return (
         <div className='shop-container'>
             <div className="review-item-container">
         {
-            cart.map(product => <ReviewItem key={product.id} product={product} handleRemoveProduct={handleRemoveProduct}></ReviewItem>)
+            cart.map(product => <ReviewItem key={product._id} product={product} handleRemoveProduct={handleRemoveProduct}></ReviewItem>)
         }
             </div>
             <div className="cart-container">
